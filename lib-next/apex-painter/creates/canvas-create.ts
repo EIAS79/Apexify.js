da5bd738ebb379c51b12151b53bfc00de2635250
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-31 01:38:03 +0200

    feat(phase4): enforce canvas validation at public boundary

--- lib-next/apex-painter/creates/canvas-create.ts
+++ lib-next/apex-painter/creates/canvas-create.ts
@@ -1,5 +1,6 @@
 import type { CanvasConfig } from "../../types";
 import { CanvasCreator } from "../../canvas/canvas-creator";
+import { validateCanvasConfig } from "../../canvas/canvas-validation";
 import type { CanvasResults } from "../../types";
 
 /** Canvas surface creation (`createCanvas`). */
@@ -7,6 +8,7 @@ export class CanvasCreate {
   constructor(private readonly creator: CanvasCreator) {}
 
   createCanvas(canvas: CanvasConfig): Promise<CanvasResults> {
+    validateCanvasConfig(canvas);
     return this.creator.createCanvas(canvas);
   }
 }
