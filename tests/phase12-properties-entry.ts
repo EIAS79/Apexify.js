0cf56f8f9ebe1b245d8e56ead224571f32263cc1
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-09-07 01:55:49 +0200

    test(phase12): add isolated property coverage fixture

---
+++ tests/phase12-properties-entry.ts
@@ -0,0 +1,6 @@
+export { classifyIpAddress } from "../lib-next/media/network-policy";
+export { assertSafeFilterExpression } from "../lib-next/video/video-text-overlay-filters";
+export { validateVideoCreationOptions } from "../lib-next/video/video-validation";
+export { resolveAssetRefsDeep } from "../lib-next/assets/asset-strings";
+export { validateSceneRenderInput } from "../lib-next/scene/scene-validation";
+export { createGradientFill } from "../lib-next/render/gradient-fill";
