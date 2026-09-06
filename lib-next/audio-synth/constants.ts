0142b2e4d33755bdbbb2742e11e896625d97c4c9
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-09-06 12:44:14 +0200

    phase9: centralize audio constants

---
+++ lib-next/audio-synth/constants.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_SAMPLE_RATE = 44_100;
+export const PCM16_BYTES_PER_SAMPLE = 2;
+export const FLOAT32_BYTES_PER_SAMPLE = Float32Array.BYTES_PER_ELEMENT;
+export const PEAK_LIMIT = 0.98;
